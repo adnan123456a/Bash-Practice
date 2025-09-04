eb45c89db57cbca22dd5f931e54522cfed7b7f97
adnan123456a <abdullahaladnan95@gmail.com>
2025-09-04 02:55:48 -0400

    Hello

---
+++ var.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+##SHowing Current Directory pwd
+
+name="Adnan"
+echo "Hello My name is $name" 
+today=$(date) # command Saved
+echo $today
+echo "Your user name is : $USER and hose name: $HOSTNAME" ## this var is builtin
+## env to see pre built var
